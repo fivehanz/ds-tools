22f1475c8afa8ee4c2bc93c9a2931e220cd13063
Haniel Ramesh <haniel56@zoho.eu>
2020-06-06 04:34:13 +0530

    new file

---
+++ linked-list/singlyLinked-2.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, item=None):
+        self.next = None
+        self.item = item
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtBegining(self):
+        pass
